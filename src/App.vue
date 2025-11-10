118fb8fc27cd1b47a93f9c16964b701a37afc499
Parisa Salimi <parisasalimi159@gmail.com>
2025-11-10 15:56:17 +0100

    Added new component and updated styles

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,77 @@
 <script setup>
 import { ref } from 'vue'
+const text = ref ('Edit me')
+const checked = ref(true)
+const checkedNames = ref(['Jack'])
+const picked = ref(['one'])
+const selected = ref('A')
+const multiSelected = ref ('A')
 
-const show = ref(true)
-const list = ref([1, 2, 3])
 </script>
-
 <template>
-  <button @click="show = !show">Toggle List</button>
-  <button @click="list.push(list.length + 1)">Push Number</button>
-  <button @click="list.pop()">Pop Number</button>
-  <button @click="list.reverse()">Reverse List</button>
+  <h1 class="title">Text Input</h1>
+  <input class="inp" v-model="text">{{ text }}</input>
 
-  <ul v-if="show && list.length">
-    <li v-for="item of list">{{ item }}</li>
-  </ul>
-  <p v-else-if="list.length">List is not empty, but hidden.</p>
-  <p v-else>List is empty.</p>
+  <h2>Checkbox</h2>
+  <input type="checkbox" id="checkbox" v-model="checked">
+  <label for="checkbox">Checked: {{ checked }}</label>
+
+  <h2>Multi Checkbox</h2>
+  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
+  <label for="jack">Jack</label>
+  <input type="checkbox" id="john" value="John" v-model="checkedNames">
+  <label for="john">John</label>
+  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
+  <label for="mike">Mike</label>
+  <input type="checkbox" id="pari" value="pari" v-model="checkedNames"></input>
+  <label for="parisa">parisa</label>
+  <p>Checked names: {{ checkedNames }}</p>
+
+  <h2>Radio</h2>
+  <input type="radio" id="one" value="One" v-model="picked">
+  <label for="one">One</label>
+  <br>
+  <input type="radio" id="two" value="Two" v-model="picked">
+  <label for="two">Two</label>
+  <br>
+  <input type="radio" id="three" value="three" v-model="picked"></input>
+  <label for="three">three</label>
+</br>
+  <p>Picked: {{ picked }}</p>
+
+  <h2>selecte</h2>
+  <select v-model="selected">
+    <option>Please select one  </option>
+
+      <option>A</option>
+      <option>B</option>
+      <option>C</option>
+  </select>
+  <p> Selected : {{ selected }}</p>
+
+  <h2>Multi Select</h2>
+  <select v-model="multiSelected" multiple style="width:100px">
+    <option>A</option>
+    <option>B</option>
+    <option>C</option>
+  </select>
+  <p>Selected: {{ multiSelected }}</p>
 </template>
+
+
+
+
+
+  
+
+<style scoped>
+
+.title{
+  margin: 0 auto;
+  
+}
+.inp{
+  margin-top: 10px;
+  margin-right: 10px;
+}
+</style>
