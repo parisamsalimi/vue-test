2f9df05592b1c7569e88f33dc535213d347cadf2
Parisa Salimi <parisasalimi159@gmail.com>
2025-11-11 15:24:34 +0100

    fetch api

--- src/App.vue
+++ src/App.vue
@@ -1,77 +1,67 @@
 <script setup>
-import { ref } from 'vue'
-const text = ref ('Edit me')
-const checked = ref(true)
-const checkedNames = ref(['Jack'])
-const picked = ref(['one'])
-const selected = ref('A')
-const multiSelected = ref ('A')
+import { ref, watchEffect } from "vue";
 
+const API_URL = `https://api.github.com/repos/vuejs/core/commits?per_page=3&sha=`;
+const branches = ["main", "minor"];
+
+const currentBranch = ref(branches[0]);
+const commits = ref([]);
+
+watchEffect(async () => {
+  const url = `${API_URL}${currentBranch.value}`;
+  commits.value = await (await fetch(url)).json();
+});
+
+function truncate(v) {
+  const newline = v.indexOf("\n");
+  return newline > 0 ? v.slice(0, newline) : v;
+}
+
+function formatDate(v) {
+  return v.replace(/T|Z/g, " ");
+}
 </script>
+
 <template>
-  <h1 class="title">Text Input</h1>
-  <input class="inp" v-model="text">{{ text }}</input>
-
-  <h2>Checkbox</h2>
-  <input type="checkbox" id="checkbox" v-model="checked">
-  <label for="checkbox">Checked: {{ checked }}</label>
-
-  <h2>Multi Checkbox</h2>
-  <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
-  <label for="jack">Jack</label>
-  <input type="checkbox" id="john" value="John" v-model="checkedNames">
-  <label for="john">John</label>
-  <input type="checkbox" id="mike" value="Mike" v-model="checkedNames">
-  <label for="mike">Mike</label>
-  <input type="checkbox" id="pari" value="pari" v-model="checkedNames"></input>
-  <label for="parisa">parisa</label>
-  <p>Checked names: {{ checkedNames }}</p>
-
-  <h2>Radio</h2>
-  <input type="radio" id="one" value="One" v-model="picked">
-  <label for="one">One</label>
-  <br>
-  <input type="radio" id="two" value="Two" v-model="picked">
-  <label for="two">Two</label>
-  <br>
-  <input type="radio" id="three" value="three" v-model="picked"></input>
-  <label for="three">three</label>
-</br>
-  <p>Picked: {{ picked }}</p>
-
-  <h2>selecte</h2>
-  <select v-model="selected">
-    <option>Please select one  </option>
-
-      <option>A</option>
-      <option>B</option>
-      <option>C</option>
-  </select>
-  <p> Selected : {{ selected }}</p>
-
-  <h2>Multi Select</h2>
-  <select v-model="multiSelected" multiple style="width:100px">
-    <option>A</option>
-    <option>B</option>
-    <option>C</option>
-  </select>
-  <p>Selected: {{ multiSelected }}</p>
+  <h1>Latest Vue Core Commits</h1>
+  <template v-for="branch in branches">
+    <input
+      type="radio"
+      :id="branch"
+      :value="branch"
+      name="branch"
+      v-model="currentBranch"
+    />
+    <label :for="branch">{{ branch }}</label>
+  </template>
+  <p>vuejs/core@{{ currentBranch }}</p>
+  <ul v-if="commits.length > 0">
+    <li v-for="{ html_url, sha, author, commit } in commits" :key="sha">
+      <a :href="html_url" target="_blank" class="commit">{{
+        sha.slice(0, 7)
+      }}</a>
+      - <span class="message">{{ truncate(commit.message) }}</span
+      ><br />
+      by
+      <span class="author">
+        <a :href="author.html_url" target="_blank">{{ commit.author.name }}</a>
+      </span>
+      at <span class="date">{{ formatDate(commit.author.date) }}</span>
+    </li>
+  </ul>
 </template>
 
-
-
-
-
-  
-
-<style scoped>
-
-.title{
-  margin: 0 auto;
-  
+<style>
+a {
+  text-decoration: none;
+  color: #42b883;
 }
-.inp{
-  margin-top: 10px;
-  margin-right: 10px;
+li {
+  line-height: 1.5em;
+  margin-bottom: 20px;
+}
+.author,
+.date {
+  font-weight: bold;
 }
 </style>
